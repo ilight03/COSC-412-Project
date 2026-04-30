1c5cb149c8b925f3fe90a2678195391b0c869655
ebhermann <emmahermann764@gmail.com>
2026-04-30 00:43:32 -0400

    Add entire backend

---
+++ backend/src/main/java/edu/widgetwizards/studentassistant/repository/NotesRepository.java
@@ -0,0 +1,19 @@
+package edu.widgetwizards.studentassistant.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import edu.widgetwizards.studentassistant.entity.NotesEntity;
+
+public interface NotesRepository extends JpaRepository<NotesEntity, Integer> {
+    
+    // Query used for finding specific notes in the database
+    // Will only return a note entity where the username is the same and the title is similar
+    // note entity is converted into a dto in the service class
+    Optional <NotesEntity> findByUsernameAndTitleLike(String username, String title);
+
+    // could alter this to return an int or long, which indicates the number of rows that were deleted
+    // could also just leave it as void though. Not sure how that would impact frontend
+    void deleteByUsernameAndTitle(String username, String title);
+}
